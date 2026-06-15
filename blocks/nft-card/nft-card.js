1805417002a846995d73d5ddae4f86ed143a0ebd
PakhawatPanpakdeewong <89290049+PakhawatPanpakdeewong@users.noreply.github.com>
2026-06-15 23:11:34 +0700

    newbie5

---
+++ blocks/nft-card/nft-card.js
@@ -0,0 +1,31 @@
+/**
+ * loads and decorates the nft-card block
+ * @param {Element} block The block element
+ */
+export default function decorate(block) {
+  block.innerHTML = `
+    <div class="card-container">
+      <a href="/" class="hero-image-container">
+        <img class="hero-image" src="https://i.postimg.cc/NfR2yhNs/image-equilibrium.jpg" alt="Spinning glass cube"/>
+      </a>
+      <main class="main-content">
+        <h1><a href="#">Equilibrium #3429</a></h1>
+        <p>Our Equilibrium collection promotes balance and calm.</p>
+        <div class="flex-row">
+          <div class="coin-base">
+            <img src="https://i.postimg.cc/T1F1K0bW/Ethereum.png" alt="Ethereum" class="small-image"/>
+            <h2>0.041 ETH</h2>
+          </div>
+          <div class="time-left">
+            <img src="https://i.postimg.cc/prpyV4mH/clock-selection-no-bg.png" alt="clock" class="small-image"/>
+            <p>3 days left</p>
+          </div>
+        </div>
+      </main>
+      <div class="card-attribute">
+        <img src="https://i.postimg.cc/SQBzNQf1/image-avatar.png" alt="avatar" class="small-avatar"/>
+        <p>Creation of <span><a href="#">Jules Wyvern</a></span></p>
+      </div>
+    </div>
+  `;
+}
---
+++ preview/fe-mentor-nft-preview-card/README.md
@@ -0,0 +1,6 @@
+# FE-Mentor NFT Preview Card
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/Laurie312/pen/PoKrYLg](https://codepen.io/Laurie312/pen/PoKrYLg).
+
